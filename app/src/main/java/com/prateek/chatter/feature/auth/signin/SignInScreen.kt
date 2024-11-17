baaaa22ddc3c3acb44be33f8df7d9420575dd90f
pki03 <khurmiprateek3@gmail.com>
2024-11-17 13:54:39 +0530

    signin and signup created

---
+++ app/src/main/java/com/prateek/chatter/feature/auth/signin/SignInScreen.kt
@@ -0,0 +1,11 @@
+package com.prateek.chatter.feature.auth.signin
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+
+fun SignInScreen(navController: NavController)
+{
+
+}
---
+++ app/src/main/java/com/prateek/chatter/feature/auth/signup/SignUpScreen.kt
@@ -0,0 +1,11 @@
+package com.prateek.chatter.feature.auth.signup
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+
+fun SignUpScreen(navController: NavController)
+{
+
+}
